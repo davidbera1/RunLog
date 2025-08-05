50f98bc3594005bbbea3dc6d5daffc41373a26cf
davidbera1 <dataaberadze@gmail.com>
2025-08-05 19:47:47 +0400

    added jvm library convention plugin

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -1,17 +1,6 @@
 plugins {
-    id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+    alias(libs.plugins.runlog.jvm.library)
 }
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-    }
-}
-
 
 dependencies {
     implementation(libs.kotlinx.coroutines.core)
