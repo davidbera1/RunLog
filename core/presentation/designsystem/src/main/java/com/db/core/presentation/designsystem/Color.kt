169b5dda38c9a86a78cc77c9cfdf3db46adc99a0
davidbera1 <dataaberadze@gmail.com>
2025-08-07 15:27:24 +0400

    feat: design system module implementation
    - added colors
    - added fonts
    - added typography
    - added icons
    - added theme

---
+++ core/presentation/designsystem/src/main/java/com/db/core/presentation/designsystem/Color.kt
@@ -0,0 +1,15 @@
+package com.db.core.presentation.designsystem
+
+import androidx.compose.ui.graphics.Color
+
+val RunLogGreen = Color(0xFF00F15E)
+val RunLogGreen30 = Color(0x4D00F15E)
+val RunLogGreen10 = Color(0x1A00F15E)
+val RunLogGreen5 = Color(0x0D00F15E)
+val RunLogBlack = Color(0xFF080707)
+val RunLogGray = Color(0xFF87938C)
+val RunLogGray40 = Color(0x6687938C)
+val RunLogDarkGray = Color(0xFF232624)
+val RunLogWhite = Color(0xFFFAFAFA)
+val RunLogDarkRed = Color(0xFFBB3D3D)
+val RunLogDarkRed5 = Color(0x0DBB3D3D)
