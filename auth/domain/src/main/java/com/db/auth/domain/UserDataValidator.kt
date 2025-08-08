f9b839b1269794345439a849f881f9d272ff6803
davidbera1 <dataaberadze@gmail.com>
2025-08-08 18:47:53 +0400

    added PatternValidator interface, added email validation function in UserDataValidator

--- auth/domain/src/main/java/com/db/auth/domain/UserDataValidator.kt
+++ auth/domain/src/main/java/com/db/auth/domain/UserDataValidator.kt
@@ -1,6 +1,13 @@
 package com.db.auth.domain
 
-class UserDataValidator {
+class UserDataValidator(
+    private val patternValidator: PatternValidator
+) {
+
+    fun isValidEmail(email: String): Boolean {
+        return patternValidator.matches(email.trim())
+    }
+
     companion object {
         const val MIN_PASSWORD_LENGTH = 9
     }
